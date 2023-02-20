822abbcde35b5b023692334947bb57bdb84e1acd
sabato85 <sankrah85@gmail.com>
2023-02-20 08:33:25 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main-A program that uses the pusts function
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+retutrn (0);
+}

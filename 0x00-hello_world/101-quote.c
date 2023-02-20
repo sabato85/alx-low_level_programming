74a65f812f3cdf7a70ae53a48e0c7470f5352163
sabato85 <sankrah85@gmail.com>
2023-02-20 15:54:08 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unisstd.h>
+/**
+*main- A program that prints a line to stanard error
+*return: 1 (success)
+*/
+int main
+{
+fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",stdout);
+return (1);
+}
